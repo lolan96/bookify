26f8f13fe12892cfbc575af786b27a0f1881f230
lolanewell <117766654+lolanewell@users.noreply.github.com>
2023-03-10 18:47:28 +0000

    workflow test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Workflow test.
         </p>
         <a
           className="App-link"
