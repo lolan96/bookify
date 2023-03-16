f04486200a400419ef4588c9aa0bd393724b0e3f
mewmew88 <mehr_hejazi@hotmail.com>
2023-03-16 20:45:50 +0000

    added "save" to Book.jsx

--- src/landing/components/Book.jsx
+++ src/landing/components/Book.jsx
@@ -1,12 +1,16 @@
-
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
 function Book({bookdata}) { 
   console.log(bookdata);
 
+function save() {
+  localStorage.setItem("bookdata", JSON.stringify(bookdata));
+}
 
   return (<div><p>{bookdata.title} </p>
   <p>{bookdata.authors[0]}</p>
+  <FontAwesomeIcon icon={faPlus} onClick ={save}/>
  <img src= {bookdata.imageLinks.smallThumbnail} alt = {bookdata.title}/>
  </div>
  )
